b93000019ba8809482e8ea725d38eac39375d5af
trapcodeio <trapcode.io@gmail.com>
2022-06-07 17:57:34 +0100

    Updated tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,23 +1,62 @@
 import { test } from "@japa/runner";
 import Users from "./users.json";
-import { populateArray } from "../index";
+import { populateArray, populateArrayAsync } from "../index";
 
-test("populateArray", async ({ assert }) => {
+type User = typeof import("./users.json")[number] & { verified?: boolean };
+
+test("populateArray", ({ assert }) => {
     // Test logic goes here
-    const users: any[] = [...Users];
+    const users: User[] = [...Users];
 
-    await populateArray(users, "verified", {
+    populateArray(users, "verified", {
         each: () => true
     });
 
     assert.isTrue(users.every((user) => user.verified === true));
 });
 
-test("populateArray: use is passed to each", async ({ assert }) => {
+test("populateArray: use is passed to each", ({ assert }) => {
     // Test logic goes here
     const users: any[] = [...Users];
 
-    await populateArray<string, Record<string, string>>(users, "email", {
+    populateArray<string, Record<string, string>>(users, "email", {
+        as: "emailUppercase",
+        use: (emails) => {
+            // test that use gets an array of the selected path
+            assert.deepEqual(
+                emails,
+                users.map((user) => user.email)
+            );
+
+            const upperCased = {} as Record<string, string>;
+            for (const email of emails) upperCased[email] = email.toUpperCase();
+
+            return upperCased;
+        },
+        each: (value, upperCased) => {
+            return upperCased[value];
+        }
+    });
+
+    assert.isTrue(users.every((user) => user.emailUppercase === user.email.toUpperCase()));
+});
+
+test("populateArrayAsync", async ({ assert }) => {
+    // Test logic goes here
+    const users: any[] = [...Users];
+
+    await populateArrayAsync(users, "verified", {
+        each: () => true
+    });
+
+    assert.isTrue(users.every((user) => user.verified === true));
+});
+
+test("populateArrayAsync: use is passed to each", async ({ assert }) => {
+    // Test logic goes here
+    const users: any[] = [...Users];
+
+    await populateArrayAsync<string, Record<string, string>>(users, "email", {
         as: "emailUppercase",
         use: (emails) => {
             // test that use gets an array of the selected path
